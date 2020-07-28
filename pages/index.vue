54d32bbc59cce90a9d97d57c9ef971d49c170da6
Richard Steen <rsteen@covermymeds.com>
2020-07-28 15:57:10 -0500

    axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
-
 <script>
+import EventCard from "@/components/EventCard.vue";
 export default {
   head() {
     return {
-      title: "Event Listing",
+      title: "Event Listing"
     };
   },
+  asyncData({ $axios }) {
+    return $axios
+      .get("http://localhost:3000/events")
+      .then(response => {
+        return {
+          events: response.data
+        };
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: "Unable to fetch events at this time, please try again"
+        });
+      });
+  },
+  components: {
+    EventCard
+  }
 };
 </script>
